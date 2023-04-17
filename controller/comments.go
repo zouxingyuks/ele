9ba9735fb1ec2e80a9f11b52b751b12d2f53fef6
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-04-18 01:31:23 +0800

    数据库输入数据安全性拦截

--- controller/comments.go
+++ controller/comments.go
@@ -2,16 +2,13 @@ package controller
 
 import "github.com/gin-gonic/gin"
 
-// CreateComment 用户评价订单
 // @Summary 用户评价订单
 // @Description 用户为指定id的订单添加评价
 // @Tags 评价管理
 // @Accept json
 // @Produce json
-// @Param order_id path int true "订单id"
-// @Param comment body CreateCommentRequest true "评论信息"
-// @Success 201 {object} CommentResponse
-// @Failure 400 {object} ErrorResponse
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
 // @Router /api/v1/comments/orders/{order_id} [post]
 func CreateComment(c *gin.Context) {
 
